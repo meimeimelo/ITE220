4a5abc51a8db25d86bf6371c7e4d0a0f7f802f73
Thiwa W <wangmeimoei@aol.com>
2015-11-23 17:27:04 +0700

    no message

--- Week2/JS/add-content.js
+++ Week2/JS/add-content.js
@@ -6,4 +6,6 @@ price = 5;
 quanlity = 14;
 total = price * quanlity;
 
-document.write("Total is" + total);
+//document.write("Total is" + total);
+var totalPriceEle= document.getElementById("totalPrice");
+totalPriceEle.textContent = "$"+ total;
