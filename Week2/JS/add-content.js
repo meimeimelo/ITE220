13fb8fea0673978374f47efb3bf592d30bea1db5
Thiwa W <wangmeimoei@aol.com>
2015-11-25 09:31:44 +0700

    no message

--- Week2/JS/add-content.js
+++ Week2/JS/add-content.js
@@ -5,7 +5,8 @@ var total = 0;
 //calculate the total price below
 var productList= document.getElementById("productList");
 productList.innerHTML += "<li>" +products[0] +": $" + price[0] +"</li>"
-
+productList.innerHTML += "<li>" +products[1] +": $" + price[0] +"</li>"
+productList.innerHTML += "<li>" +products[2] +": $" + price[0] +"</li>"
 
 //Display purchase product to the screen
 total = price[0];
