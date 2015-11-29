7e8a5060e1c9f9f0d7d1b405688e0a8669ddf4df
Thiwa W <wangmeimoei@aol.com>
2015-11-29 18:56:35 +0700

    no message

--- Week2-2/js/calendar.js
+++ Week2-2/js/calendar.js
@@ -10,7 +10,7 @@ for ( var i = 1; i <= dayInMonth; i++) {
       stringHTML += "<tr>";
       count++
   }
-  if( count === 6 ) {
+  if( count === 7 ) {
     stringHTML += "</tr>";
     count = 0;
   }
