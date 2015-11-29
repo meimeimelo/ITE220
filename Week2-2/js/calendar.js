e2c4a0fa04f372543fbb90cf0f380993bee1cf9a
Thiwa W <wangmeimoei@aol.com>
2015-11-29 18:55:16 +0700

    no message

--- Week2-2/js/calendar.js
+++ Week2-2/js/calendar.js
@@ -2,5 +2,20 @@
 var hotelTable = document.getElementById("hotelCalendarTable");
 var defaultData = hotelTable.innerHTML;
 var stringHTML = "";
-stringHTML = "<tr><td>1</td><td>2</td></tr>";
+var dayInMonth = 31;
+var count = 0;
+
+for ( var i = 1; i <= dayInMonth; i++) {
+  if( count === 0 ) {
+      stringHTML += "<tr>";
+      count++
+  }
+  if( count === 6 ) {
+    stringHTML += "</tr>";
+    count = 0;
+  }
+  else count ++;
+  stringHTML += "<td>" + i + "</td>";
+}
+// stringHTML = "<tr><td>1</td><td>2</td></tr>";
 hotelTable.innerHTML += stringHTML;
---
+++ Week2-2/js/object-literal.js
@@ -0,0 +1,9 @@
+var hotel = {
+  name: 'Meimei',
+  room: 3000,
+  booked: 2000,
+
+  checkAvailability: function(){
+    return this.room - this.booked;
+  }
+}
