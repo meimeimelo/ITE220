cfc432a9460c39ccebd57fbfbd6b048d32d1c61d
Thiwa W <wangmeimoei@aol.com>
2015-11-29 18:12:41 +0700

    no message

--- Week2/JS/add-content.js
+++ Week2/JS/add-content.js
@@ -11,7 +11,7 @@ productList.innerHTML += "<li>" +products[2] +": $" + price[2] +"</li>"
 //Display purchase product to the screen
 // total = price[0] + price[1] + price[2];
 total = price.reduce(function(prev, curr) { return prev + curr });
-total = total - (total * 0.2);
+total -= total * 0.2;
 
 //document.write("Total is" + total);
 var totalPriceEle= document.getElementById("totalPrice");
