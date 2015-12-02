87deeb7afcf46b13aa993ee03a81316c83b99eb5
Thiwa W <wangmeimoei@aol.com>
2015-12-02 18:08:04 +0700

    no message

--- Week3/js/window-example.js
+++ Week3/js/window-example.js
@@ -1,6 +1,8 @@
 var myWindow = {
 	screenX: window.screenX,
 	screenY: window.screenY,
+	screenW: window.screen.width,
+	screenH: window.screen.height,
 	location: window.location.pathname,
 
 	render: function() {
@@ -9,6 +11,15 @@ var myWindow = {
 
 		ele = document.getElementById("sx");
 		ele.value = this.screenX;
+
+		ele = document.getElementById("sy");
+		ele.value = this.screenY;
+
+		ele = document.getElementById("sw");
+		ele.value = this.screen.width;
+
+		ele = document.getElementById("sh");
+		ele.value = this.screen.height;
 	}
 };
 
