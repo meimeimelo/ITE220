baed257d7b8a9b7ad3058bf8b8159e81e0534efd
Thiwa W <wangmeimoei@aol.com>
2015-12-02 23:44:49 +0700

    no message

--- Week3/js/game.js
+++ Week3/js/game.js
@@ -1,7 +1,9 @@
 var game = {
-	answer: 50, // Need to random this value from 0-100
+	answer: Math.round(Math.random() * (100 - 1)),
+	value : '',
 	init: function() {
 		var self = this;
+
 		// Number 1
 		var ele = document.getElementById("one");
 		ele.addEventListener("click", function(){
@@ -14,63 +16,85 @@ var game = {
 			self.click(2);
 		}, false);
 
-		//Number3
+		// Number 3
 		var ele = document.getElementById("three");
 		ele.addEventListener("click", function(){
 			self.click(3);
 		}, false);
 
-		//Number4
+		// Number 4
 		var ele = document.getElementById("four");
 		ele.addEventListener("click", function(){
 			self.click(4);
 		}, false);
 
-		//Number 5
+		// Number 5
 		var ele = document.getElementById("five");
 		ele.addEventListener("click", function(){
 			self.click(5);
 		}, false);
 
-		//Number 6
+		// Number 6
 		var ele = document.getElementById("six");
 		ele.addEventListener("click", function(){
 			self.click(6);
 		}, false);
 
-		//Number 7
-			var ele = document.getElementById("seven");
-			ele.addEventListener("click",function(){
-				self.click(7);
-			}, false);
+		// Number 7
+		var ele = document.getElementById("seven");
+		ele.addEventListener("click", function(){
+			self.click(7);
+		}, false);
 
-			//Number 8
-			var ele = document.getElementById("eight");
-			ele.addEventListener("click", function(){
-				self.click(8);
-			}, false);
+		// Number 8
+		var ele = document.getElementById("eight");
+		ele.addEventListener("click", function(){
+			self.click(8);
+		}, false);
 
-			//Number 9
-			var ele = document.getElementById("nine");
-			ele.addEventListener("click", function(){
-				self.click(9);
-			}, false);
+		// Number 9
+		var ele = document.getElementById("nine");
+		ele.addEventListener("click", function(){
+			self.click(9);
+		}, false);
+
+		// Number 0
+		var ele = document.getElementById("zero");
+		ele.addEventListener("click", function(){
+			self.click(0);
+		}, false);
 
 		// Guess!
 		var ele = document.getElementById("guess");
 		ele.addEventListener("click", this.checkAnswer, false);
+
+		// Show Answer
+		var ele = document.getElementById("show");
+		ele.addEventListener("click", this.showAnswer, false);
+
+		// Clear Answer
+		var ele = document.getElementById("clear");
+		ele.addEventListener("click", this.clearAnswer, false);
 	},
 	click: function(value) {
 		console.log(value + " clicked.");
 		var userValueEle = document.getElementById("userValue");
 		userValueEle.value += value;
+		this.value += value;
 	},
 	checkAnswer: function() {
-		alert("This is not correct! Try again. ");
-	}
-
-	guess: function(){
-		alert("")
+		if (parseInt(game.value) === game.answer) alert("Congratulations! Correct Answer ");
+		else if (parseInt(game.value) < game.answer)alert("Guessing number is less than the correct number");
+		else if (parseInt(game.value) > game.answer)alert("Guessing number is more than the correct number");
+	},
+	showAnswer: function() {
+		alert(game.answer);
+	},
+	clearAnswer: function() {
+		var userValueEle = document.getElementById("userValue");
+		userValueEle.value = '';
+		game.value = '';
+		console.log(userValueEle.value)
 	}
 };
 
