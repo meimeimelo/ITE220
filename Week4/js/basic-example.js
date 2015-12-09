14ba532a0dd7a3dc5b3a8ee0274b124fde51106b
Thiwa W <wangmeimoei@aol.com>
2015-12-09 09:27:09 +0700

    no message

--- Week4/js/basic-example.js
+++ Week4/js/basic-example.js
@@ -1,1 +1,1 @@
-$(':header').addClass('headline');$('li:lt(3)').hide();$('li').on('click', function() {  $(this).remove();});$(':header').addClass('lion');$('li:lt(4)').show();$.('li')on('click', function(){  $(this).remove();});
+$(':header').addClass('headline');$('li:lt(3)').hide();$('li').on('click', function() {  $(this).remove();});$(':header').addClass('lion');$.('li')on('click', function(){  $('li:lt(4)').show();});
