bd56ac242a4bb362cf217dd1f2b49b1b510f6cb9
Thiwa W <wangmeimoei@aol.com>
2015-12-16 08:58:20 +0700

    no message

--- Week5/js/app.js
+++ Week5/js/app.js
@@ -5,8 +5,10 @@ $(function(){
 			app.init_event();
 		},
 
+// '>' means direct into that line
 		init_effect: function() {
 			$("#head_tagline>h1").hide().fadeIn(700);
+			$("#head_tagline>p:first").hide().fadeIn(700);
 		},
 
 		init_event: function() {
