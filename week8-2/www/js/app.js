d6771d215ab4907c5f8268476e3ed2b45f25f8d4
Thiwa W <wangmeimoei@aol.com>
2016-01-22 16:10:06 +0700

    no message

--- week8-2/www/js/app.js
+++ week8-2/www/js/app.js
@@ -9,30 +9,7 @@ $(function(){
 		init_effect: function() {
 			$(".information>h4").hide().fadeIn(700);
 			$(".information>h7").hide().delay(700).fadeIn(700);
-			//hide the error
-			// $(".error").hide();
 		}
-
-		// init_event: function() {
-		// 	$("#head_tagline>a:first").on("click",function(){
-		// 		$("#head_tagline").height(350);
-		// 		$("#head_tagline>p:last").fadeIn(700);
-		// 	});
-		//
-		// 	$("#head_tagline>a:last").on("click",function(){
-		// 		$("#head_tagline").height(200);
-		// 		$("#head_tagline>p:last").hide();
-		// 	});
-
-
-
-			//image flip
-			// $(".image-flip").flip({
-			// 	axis: 'y',
-			// 	trigger: 'hover'
-			// });
-
-		// }
 	};
 app.init();
 });
